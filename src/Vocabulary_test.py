f9fb251cc11b10428ecbe56abf893d6a6aa7e939
Shang-Yu Chan <87119388+5432roy@users.noreply.github.com>
2022-10-05 22:48:15 -0700

    turn vocabulary_test.py into a main function holder

--- src/Vocabulary_test.py
+++ src/Vocabulary_test.py
@@ -1,47 +1,7 @@
-import requests
-
-def get_token(path):
-    try:
-        file = open(path)
-        token = file.readline().strip()
-        database_ID = file.readline().strip()
-        return token, database_ID
-    except:
-        raise FileNotFoundError
-
-def read_database():
-
-    token, database_ID = get_token("D:/Visual Studio Code/GitHub/Vocabulary_Test/src/token.txt")
-
-    readURL = f'https://api.notion.com/v1/databases/{database_ID}/query'
-    headers = {
-            "Authorization": token,
-            "Notion-Version": "2022-06-28"
-    }
-    try:
-        res = requests.request("POST", readURL, headers = headers)
-        data = res.json()
-        return data
-    except:
-        print("Fetch Error")
-
-def data_parser(data):
-    vocabularies = {}
-    for vocabulary in data['results']:
-        cur = vocabulary['properties']
-        if len(cur['Meaning']['rich_text']) > 0:
-            vocabularies[cur['Vocabulary']['title'][-1]['plain_text']] = cur['Meaning']['rich_text'][-1]['plain_text']
-    return vocabularies
-        
-
-data = read_database()
-vocabularies = data_parser(data)
-print(vocabularies)
+from GUI import *
+from fetch_from_notion import *
 
 
-"""
-print(data['results'][1]['properties']['Vocabulary']['title'][-1]['plain_text'])
-print(data['results'][1]['properties']['Meaning']['rich_text'][-1]['plain_text'])
-print(data['results'][1]['properties']['Meaning 2'])
-"""
+
+
 
